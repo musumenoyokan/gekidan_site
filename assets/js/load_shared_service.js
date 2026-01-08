bbb54effc6a61dde70bc1604569b2d66046b2890
engeki <musumenoyokan@gmail.com>
2026-01-08 16:40:53 +0900

    2月公演情報掲載

--- assets/js/load_shared_service.js
+++ assets/js/load_shared_service.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     fetch('/shared_service.html')
         .then(response => {
             if (!response.ok) {
